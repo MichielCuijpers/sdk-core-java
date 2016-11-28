18e4dbf8d7e133ea294494245b8e81617eb72c73
Andrea Rizzini <andrea_rizzini@mastercard.com>
2016-11-28 15:06:06 +0000

    arizzini: disable tests..

--- src/test/groovy/com/mastercard/api/core/functional/NodeJSMockServerSpec.groovy
+++ src/test/groovy/com/mastercard/api/core/functional/NodeJSMockServerSpec.groovy
@@ -18,7 +18,7 @@ import spock.lang.Specification
  */
 
 
-//@IgnoreIf({ System.getProperty("RUN_NODEJS") == null })
+@IgnoreIf({ System.getProperty("RUN_NODEJS") == null })
 class NodeJSMockServerSpec extends Specification {
 
 
