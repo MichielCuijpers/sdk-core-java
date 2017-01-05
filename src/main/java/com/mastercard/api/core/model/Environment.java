114b109aac7a7d5d1dfd55a48ae2a4f74f2b77bd
Andrea Rizzini <andrea_rizzini@mastercard.com>
2017-01-05 11:56:44 +0000

    arizzini: adding a new environment needed for MDES sandbox.

--- src/main/java/com/mastercard/api/core/model/Environment.java
+++ src/main/java/com/mastercard/api/core/model/Environment.java
@@ -7,12 +7,13 @@ import java.util.Map;
  * Created by andrearizzini on 22/11/2016.
  */
 public enum Environment {
-    PRODUCTION,SANDBOX,STAGE,DEV,PRODUCTION_MTF,PRODUCTION_ITF,STAGE_MTF,STAGE_ITF,LOCALHOST,OTHER;
+    PRODUCTION,SANDBOX,SANDBOX_STATIC,STAGE,DEV,PRODUCTION_MTF,PRODUCTION_ITF,STAGE_MTF,STAGE_ITF,LOCALHOST,OTHER;
 
     public static final Map<Environment,String[]> MAPPINGS = new HashMap();
     static {
         MAPPINGS.put(Environment.PRODUCTION, new String[] { "https://api.mastercard.com", null});
         MAPPINGS.put(Environment.SANDBOX, new String[] { "https://sandbox.api.mastercard.com", null});
+        MAPPINGS.put(Environment.SANDBOX_STATIC, new String[] { "https://sandbox.api.mastercard.com", "static"});
         MAPPINGS.put(Environment.STAGE, new String[] { "https://stage.api.mastercard.com", null});
         MAPPINGS.put(Environment.PRODUCTION_MTF, new String[] { "https://api.mastercard.com", "mtf"});
         MAPPINGS.put(Environment.PRODUCTION_ITF, new String[] { "https://api.mastercard.com", "itf"});
