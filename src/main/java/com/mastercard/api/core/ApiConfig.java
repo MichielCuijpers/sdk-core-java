62755da2086ed8efc4024e94ca826c52f6b31f24
Rizzini, Andrea <Andrea.Rizzini@mastercard.com>
2017-08-29 11:16:10 +0100

    arizzini: adding test to for UTF8

--- src/main/java/com/mastercard/api/core/ApiConfig.java
+++ src/main/java/com/mastercard/api/core/ApiConfig.java
@@ -93,9 +93,6 @@ public final class ApiConfig {
              */
 
 
-            java.util.logging.Logger.getLogger("org.apache.http.wire").setLevel(java.util.logging.Level.FINEST);
-            java.util.logging.Logger.getLogger("org.apache.http.headers").setLevel(java.util.logging.Level.FINEST);
-
             System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
             System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
             System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "DEBUG");
